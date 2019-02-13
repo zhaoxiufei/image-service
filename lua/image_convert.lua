313eb1141ab54be11fd95d5b1fff8a9942990c5d
zhaoxiufei@gmail.com <zhaoxiufei@gmail.com>
2019-02-13 16:05:34 +0800

    init

--- lua/image_convert.lua
+++ lua/image_convert.lua
@@ -1,18 +1,20 @@
-
 local magick = require('magick')
 local fastdfs = require('restyfastdfs')
+local tonumber = tonumber
 
 -- http://192.168.5.20/group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png?s=thumb&a=400x600
 -- fastdfs 配置
 local fdfs_tracker = '192.168.5.20'
+local fdfs_tracker_port = tonumber(22122)
 
 local image_root = '/home/magic/images'
+local image_404 =image_root .. '/404.png'
 local uri = ngx.var.uri
-ngx.log(ngx.INFO, 'uri:[', uri, ']') -- /group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png
+--ngx.log(ngx.INFO, 'uri:[', uri, ']') -- /group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png
 local request_uri = ngx.var.request_uri
-ngx.log(ngx.INFO, 'request_uri:[', request_uri, ']') -- /group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png?s=thumb&a=400x600
+--ngx.log(ngx.INFO, 'request_uri:[', request_uri, ']') -- /group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png?s=thumb&a=400x600
 local source = image_root .. uri
-ngx.log(ngx.INFO, 'source:[', source, ']') -- /home/magic/images/group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png
+--ngx.log(ngx.INFO, 'source:[', source, ']') -- /home/magic/images/group1/M00/00/0B/wKgFFFxidyGAR_YlAAMWCLrVDyk231.png
 
 -- 最后一个索引位置
 function findLast(haystack, needle)
@@ -44,12 +46,25 @@ function file_exists(name)
     if f ~= nil then io.close(f) return true else return false end
 end
 
+-- 返回结果
+function response(image_name)
+    local img = magick.load_image(image_name)
+    ngx.say(img:get_blob())
+    img:destroy()
+end
+
+function responseAndWrite(img, image_name)
+    ngx.say(img:get_blob())
+    img:write(image_name)
+    img:destroy()
+end
+
 local image_dir = image_root .. string.sub(uri, 0, findLast(uri, '/')) -- /home/magic/images/group1/M00/00/0B/
-ngx.log(ngx.INFO, 'image_dir:[', image_dir, ']')
+--ngx.log(ngx.INFO, 'image_dir:[', image_dir, ']')
 if not file_exists(source) then
     local file_id = string.sub(uri, 2)
     local fdfs = fastdfs:new()
-    fdfs:set_tracker(fdfs_tracker, 22122)
+    fdfs:set_tracker(fdfs_tracker, fdfs_tracker_port)
     fdfs:set_timeout(1000)
     fdfs:set_tracker_keepalive(0, 100)
     fdfs:set_storage_keepalive(0, 100)
@@ -60,59 +75,54 @@ if not file_exists(source) then
             os.execute('mkdir -p ' .. image_dir)
         end
         writefile(source, data)
+    else
+        response(image_404)
+        ngx.exit(0)
     end
 end
 
 local args = ngx.req.get_uri_args()
 local image_service = args['s']
-ngx.log(ngx.INFO, 'image service:[', image_service, ']') -- thumb
+--ngx.log(ngx.INFO, 'image service:[', image_service, ']') -- thumb
+local cache_key = image_root .. request_uri
+--ngx.log(ngx.INFO, 'cache_key:', cache_key)
 if image_service == nil then
     -- 获取原图
-    -- local file = io.open(original_file);
-    -- local source_image = file:read('*all')
-    -- file:close()
-    ngx.log(ngx.INFO, 'get source image!')
-    -- ngx.say(source_image)
-    local img = magick.load_image(source)
-    ngx.say(img:get_blob())
+    response(source)
 elseif image_service == 'thumb' then
     -- thumb 裁切（缩放）
-    -- args
-    local image_action = args['a']
-    local image_name
-    -- get from cache
-    local cache_key = image_root .. ngx.var.request_uri
-    ngx.log(ngx.INFO, 'cache_key:', cache_key)
     if file_exists(cache_key) then
         ngx.log(ngx.INFO, image_service, ', cache hit! cache key: ', cache_key)
-        image_name = source
     else
-        magick.thumb(source, image_action, cache_key)
-        image_name = cache_key
+        ngx.log(ngx.INFO, 'cache miss, exec thumb:[', args['a'], ']')
+        magick.thumb(source, args['a'], cache_key)
     end
-    local img = magick.load_image(cache_key)
-    ngx.say(img:get_blob())
+    response(cache_key)
 elseif image_service == 'resize' then
     -- 图片拉伸
-    -- args, 目标宽和高
-    local width = tonumber(args['w'])
-    local height = tonumber(args['h'])
-
-    -- source info
-    local img = magick.load_image(source)
-    ngx.log(ngx.INFO, 'source width:', img:get_width(), 'source height:', img:get_height());
-    ngx.log(ngx.INFO, image_service, ', arg, width:', width, ', height:', height)
-    img:resize(width, height)
-    ngx.say(img:get_blob())
+    if file_exists(cache_key) then
+        ngx.log(ngx.INFO, image_service, ', cache hit! cache key: ', cache_key)
+        response(cache_key)
+    else
+        -- args, 目标宽和高
+        ngx.log(ngx.INFO, 'cache miss, exec resize:[', args['w'], args['h'], ']')
+        local img = magick.load_image(source)
+        img:resize(tonumber(args['w']), tonumber(args['h']))
+        responseAndWrite(img, cache_key)
+    end
 
 elseif image_service == 'rotate' then
     -- 图片旋转
-    -- args, 旋转度数
-    local degrees = tonumber(args['d'])
-
-    local img = magick.load_image(source)
-    img:rotate(degrees)
-    ngx.say(img:get_blob())
+    if file_exists(cache_key) then
+        ngx.log(ngx.INFO, image_service, ', cache hit! cache key: ', cache_key)
+        response(cache_key)
+    else
+        -- args, 旋转度数
+        ngx.log(ngx.INFO, 'cache miss, exec rotate:[', args['d'], ']')
+        local img = magick.load_image(source)
+        img:rotate(tonumber(args['d']))
+        responseAndWrite(img, cache_key)
+    end
 else
     ngx.say('unknow image service!')
 end
